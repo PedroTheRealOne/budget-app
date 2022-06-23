61c049f3e5895c34d4a4ff9adafacf8b50d697e0
Pedro <pedrotherealone1@gmail.com>
2022-06-23 13:21:24 -0300

    feat: add avaliable budget

--- app/helpers/transaction_helper.rb
+++ app/helpers/transaction_helper.rb
@@ -2,4 +2,18 @@ module TransactionHelper
     def transaction_class(transaction)
         transaction.transaction_date > Date.today ? "bg-warning" : ""
     end
+
+    def transaction_budget_avaliable(transactions)
+        income = 0
+        outcome = 0
+        transactions.each do |n|
+            if n.transaction_type.id == 1 
+                income += n.value
+            else
+                outcome += n.value
+            end
+        end
+
+        return income - outcome
+    end
 end
